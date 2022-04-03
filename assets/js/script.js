81fef0d3768807de1babd0640e096b26067d1aab
Joe R <joeryansemail@gmail.com>
2022-04-03 16:53:05 -0400

    save button added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
 // Display today's day and date
 var todaysDate = moment().format('dddd, MMM Do YYYY');
     $("#currentDay").html(todaysDate);
+
+// Saves time and values to local storage
+$(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".time-block").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    localStorage.setItem(time, value);
+  });
+
